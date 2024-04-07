1a207bc39ef2117b88f9161c72fb472f00dabb2d
Aimarekin <aimarekin@gmail.com>
2024-04-07 18:24:40 +0200

    That was kinda useless so I transformed it into something else (2.0.0 lol)

--- src/index.ts
+++ src/index.ts
@@ -3,39 +3,39 @@ export type StringOrURIArrayable = StringOrURI | StringOrURI[];
 export type SpicetifyURIType = (typeof Spicetify.URI.Type)[keyof typeof Spicetify.URI.Type];
 
 /**
- * Coerces the input into a Spicetify URI.
+ * Asserts that the input is an object that is/can be transformed into a Spicetify URI.
  *
- * @param uri - The input to be coerced into a Spicetify URI.
- * @returns The coerced Spicetify URI.
- * @throws {Error} if the input is an invalid URI.
+ * @param uri - The input to be asserted as a Spicetify URI.
+ * @returns The Spicetify URI.
+ * @throws {Error} If the input is not a valid Spicetify URI.
  */
-export function coerceIntoURI(uri: StringOrURI): Spicetify.URI;
-export function coerceIntoURI(uri: any): never;
-export function coerceIntoURI(uri: any): Spicetify.URI | never {
-	const coercedURI = typeof uri === "string" ? Spicetify.URI.from(uri) : uri;
-	if (coercedURI === null) {
-		throw new Error("Invalid URI");
+export function assertURI(uri: StringOrURI): Spicetify.URI;
+export function assertURI(uri: any): never;
+export function assertURI(uri: any): Spicetify.URI | never {
+	uri = Spicetify.URI.from(uri);
+	if (!(uri instanceof Spicetify.URI)) {
+		throw new Error("Object is not a Spicetify.URI instance");
 	}
 
-	return coercedURI;
+	return uri;
 }
 
 /**
- * Coerces the input into an array of Spicetify.URI objects.
- * If the input is not an array, it will be wrapped in an array.
+ * Asserts that the input is an array of objects that are/can be transformed into Spicetify URIs.
+ * If the input is not an array, it will be converted into an array with a single element.
  *
- * @param uris - The input to be coerced into an array of Spicetify.URI objects.
- * @returns An array of Spicetify.URI objects.
- * @throws {Error} If the input is not an array or cannot be coerced into a Spicetify.URI object.
+ * @param uris - The input to be asserted as an array of Spicetify URIs or a single Spicetify URI.
+ * @returns An array of Spicetify URIs.
+ * @throws {Error} If the input is not a valid Spicetify URI.
  */
-export function coerceIntoURIArray(uris: StringOrURIArrayable): Spicetify.URI[];
-export function coerceIntoURIArray(uris: any): never;
-export function coerceIntoURIArray(uris: any): Spicetify.URI[] | never {
+export function assertURIArray(uris: StringOrURIArrayable): Spicetify.URI[];
+export function assertURIArray(uris: any): never;
+export function assertURIArray(uris: any): Spicetify.URI[] | never {
 	if (!Array.isArray(uris)) {
 		uris = [uris];
 	}
 
-	return uris.map(coerceIntoURI);
+	return uris.map((uri: any) => assertURI(uri));
 }
 
 /**
@@ -92,7 +92,5 @@ export function assertURIArrayType(URIArray: any[], validType: Parameters<typeof
 		throw new Error("Object is not an array");
 	}
 
-	URIArray.forEach((uri) => assertURIType(uri, validType));
-
-	return URIArray;
+	return URIArray.map((uri) => assertURIType(uri, validType));
 }
